0231b89a6571594ce06efa8748b08618c0bc00d8
Anirudh Vemula <vvanirudh@gmail.com>
2022-01-08 16:27:08 -0500

    trying out a small model class; evaluating loss only once

--- src/agent/mountain_car/mountain_car_agnostic_sysid_agent.jl
+++ src/agent/mountain_car/mountain_car_agnostic_sysid_agent.jl
@@ -15,14 +15,15 @@ function MountainCarAgnosticSysIDAgent(
 end
 
 function generate_model_class()::Array{MountainCarParameters}
-    theta1s = -0.0024:-0.00001:-0.0026
-    theta2s = 2.9:0.01:3.1
-    models = []
-    for theta1 in theta1s
-        for theta2 in theta2s
-            push!(models, MountainCarParameters(theta1, theta2))
-        end
-    end
+    # theta1s = -0.0024:-0.00001:-0.0026
+    # theta2s = 2.9:0.01:3.1
+    # models = []
+    # for theta1 in theta1s
+    #     for theta2 in theta2s
+    #         push!(models, MountainCarParameters(theta1, theta2))
+    #     end
+    # end
+    models = [MountainCarParameters(-0.0025, 3), MountainCarParameters(-0.00235, 3.09375)]
     models
 end
 
@@ -32,13 +33,15 @@ function run(
 )
     rng = MersenneTwister(0)
     # Choose an initial model
-    params = MountainCarParameters(-0.002, 3.5)
+    params = true_params
     # Choose an initial policy
     policy, values, _ = value_iteration(agent.model, params)
     # Initialize dataset
     all_transitions::Array{Array{MountainCarContTransition}} = []
     # Initialize value functions
     all_values::Array{Array{Float64}} = []
+    # Initialize losses
+    losses = [0.0 for _ in 1:length(agent.model_class)]
 
     # Start iterations
     n = 1000  # number of iterations
@@ -69,21 +72,20 @@ function run(
         # Store value function
         push!(all_values, values)
         # Collected data, now update model
-        losses = []
-        for model_params in agent.model_class
-            # TODO: Need to do something more FTRL like
+        for j in 1:length(agent.model_class)
             # Compute loss
             loss = compute_model_advantage_loss(
                 agent,
-                all_transitions,
-                all_values,
-                model_params
+                all_transitions[i],
+                all_values[i],
+                agent.model_class[j],
             )
-            loss = loss / (i * (m + p))
+            loss = loss / (m + p)
             # println(vec(model_params), " ", loss)
-            push!(losses, loss)
+            losses[j] += loss
         end
         # Find best model
+        # TODO: Doing FTL now, but need to do better
         params = agent.model_class[argmin(losses)]
         println(vec(params), " ", minimum(losses))
         # Compute corresponding policy and values
@@ -93,36 +95,32 @@ end
 
 function compute_model_advantage_loss(
     agent::MountainCarAgnosticSysIDAgent,
-    all_transitions::Array{Array{MountainCarContTransition}},
-    all_values::Array{Array{Float64}},
+    transitions::Array{MountainCarContTransition},
+    values::Array{Float64},
     model_params::MountainCarParameters,
 )
     loss = 0.0
-    for j in 1:length(all_transitions)
-        iteration_transitions = all_transitions[j]
-        iteration_values = all_values[j]
-        for transition in iteration_transitions
-            predicted_state, _ = step(
-                agent.model,
-                transition.initial_state,
-                transition.action,
-                model_params,
-            )
-            loss += abs(
-                iteration_values[
-                    cont_state_to_idx(
-                        agent.model,
-                        transition.final_state,
-                    )
-                ] -
-                iteration_values[
-                    cont_state_to_idx(
-                        agent.model,
-                        predicted_state,
-                    )
-                ]
-            )
-        end
+    for transition in transitions
+        predicted_state, _ = step(
+            agent.model,
+            transition.initial_state,
+            transition.action,
+            model_params,
+        )
+        loss += abs(
+            values[
+                cont_state_to_idx(
+                    agent.model,
+                    transition.final_state,
+                )
+            ] -
+            values[
+                cont_state_to_idx(
+                    agent.model,
+                    predicted_state,
+                )
+            ]
+        )
     end
     loss
 end
